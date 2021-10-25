6e5986d0d9daf38fc282e1aa6f7fd020f44712ae
taylor <taylormaruca@yahoo.com>
2021-10-25 14:45:20 -0400

    updated details.tsx

--- rachelsrecipes/src/components/Details.tsx
+++ rachelsrecipes/src/components/Details.tsx
@@ -1,31 +1,18 @@
 //children: JSX.Element,
-import React from "react";
+import React, { FC } from "react";
 import { Recipe } from "../models/recipe-model";
-import { Link } from "react-router-dom"; {/* added Link */ }
+import { Link } from "react-router-dom";import { JsxEmit } from "typescript";
+ {/* added Link */ }
 
 
-interface Props {
-    recipes: Recipe[];
-}
-
-function Details({recipes}: Props) {
-    return (
-      <>
-      <h2>Recipe Details</h2>
-      {recipes.map((hits) => {
-        return (
-            <div key = {hits.recipe.label} >
-              <div className="RecipeDetails">
-                {hits.recipe.label}
-                {hits.recipe.mealType}
-                {hits.recipe.cuisineType}
-              </div>
-            </div>
-        );
-      })}
-      <p><Link to="/"><button>Back to Home</button></Link></p>
-      </>
-    )};
+const Details: FC<Recipe> = ({ recipe }): JSX.Element => {
+  return <>
+    <p>Title: {recipe.label}</p>
+    <p>Meal Type: {recipe.mealType}</p>
+    <p>Cuisine Type: {recipe.cuisineType}</p>
+    <p>Calories: {recipe.calories}</p>
+  </>;
+};
 
 // Link to see the recipe on their original source site
 //Favorites button
@@ -33,4 +20,3 @@ function Details({recipes}: Props) {
 
 
 export default Details;
-
--- rachelsrecipes/src/components/Results.tsx
+++ rachelsrecipes/src/components/Results.tsx
@@ -20,6 +20,8 @@ function Results({recipes}: Props) {
               <img src={hits.recipe.image}  width="250" height="250"/>
               <h3 id = "recipeResultsName">{hits.recipe.label}</h3>
               <p id="details"><Link to="/details">More Details</Link></p>
+              <p>See the the full recipe on <b>{hits.recipe.source}</b></p>
+              <a href={hits.recipe.url}>Click Here</a>
             <button className="addButton" onClick={() => addFavorite(hits)}>Add to Favorites</button>
             </div>
           </div>
